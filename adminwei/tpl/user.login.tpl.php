7e1032f92955fb7cf678e319cda59170ef345890
haidao17 <yiwenqi1989@gmail.com>
2015-12-30 16:51:35 +0800

    后台登陆页面修改完成

--- adminwei/tpl/user.login.tpl.php
+++ adminwei/tpl/user.login.tpl.php
@@ -4,130 +4,8 @@
 <head>
 <html xmlns="http://www.w3.org/1999/xhtml"><head><meta content="IE=11.0000" http-equiv="X-UA-Compatible">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
-<title>YYGCMS后台登陆</title>
+<title>后台登陆</title>
 <script src="<?php echo G_GLOBAL_STYLE; ?>/global/login/js/jquery-1.9.1.min.js" type="text/javascript"></script>
-<style>
-body{
-	background: #ebebeb;
-	font-family: "Helvetica Neue","Hiragino Sans GB","Microsoft YaHei","\9ED1\4F53",Arial,sans-serif;
-	color: #222;
-	font-size: 12px;
-}
-*{padding: 0px;margin: 0px;}
-.top_div{
-	background: #008ead;
-	width: 100%;
-	height: 400px;
-}
-.ipt{
-	border: 1px solid #d3d3d3;
-	padding: 10px 10px;
-	width: 290px;
-	border-radius: 4px;
-	padding-left: 35px;
-	-webkit-box-shadow: inset 0 1px 1px rgba(0,0,0,.075);
-	box-shadow: inset 0 1px 1px rgba(0,0,0,.075);
-	-webkit-transition: border-color ease-in-out .15s,-webkit-box-shadow ease-in-out .15s;
-	-o-transition: border-color ease-in-out .15s,box-shadow ease-in-out .15s;
-	transition: border-color ease-in-out .15s,box-shadow ease-in-out .15s
-}
-.ipt:focus{
-	border-color: #66afe9;
-	outline: 0;
-	-webkit-box-shadow: inset 0 1px 1px rgba(0,0,0,.075),0 0 8px rgba(102,175,233,.6);
-	box-shadow: inset 0 1px 1px rgba(0,0,0,.075),0 0 8px rgba(102,175,233,.6)
-}
-.u_logo{
-	background: url("<?php echo G_GLOBAL_STYLE; ?>/global/login/images/username.png") no-repeat;
-	padding: 10px 10px;
-	position: absolute;
-	top: 43px;
-	left: 66px;
-
-}
-.p_logo{
-	background: url("<?php echo G_GLOBAL_STYLE; ?>/global/login/images/password.png") no-repeat;
-	padding: 10px 10px;
-	position: absolute;
-	top: 12px;
-	left: 66px;
-}
-.code{
-	padding: 10px 10px;
-	position: absolute;
-	top: 12px;
-	left: 40px;
-}
-.coder
-{
-margin: 200px;
-
-
-}
-a{
-	text-decoration: none;
-}
-.tou{
-	background: url("<?php echo G_GLOBAL_STYLE; ?>/global/login/images/tou.png") no-repeat;
-	width: 97px;
-	height: 92px;
-	position: absolute;
-	top: -87px;
-	left: 140px;
-}
-.left_hand{
-	background: url("<?php echo G_GLOBAL_STYLE; ?>/global/login/images/left_hand.png") no-repeat;
-	width: 32px;
-	height: 37px;
-	position: absolute;
-	top: -38px;
-	left: 150px;
-}
-.right_hand{
-	background: url("<?php echo G_GLOBAL_STYLE; ?>/global/login/images/right_hand.png") no-repeat;
-	width: 32px;
-	height: 37px;
-	position: absolute;
-	top: -38px;
-	right: -64px;
-}
-.initial_left_hand{
-	background: url("<?php echo G_GLOBAL_STYLE; ?>/global/login/images/hand.png") no-repeat;
-	width: 30px;
-	height: 20px;
-	position: absolute;
-	top: -12px;
-	left: 100px;
-}
-.initial_right_hand{
-	background: url("<?php echo G_GLOBAL_STYLE; ?>/global/login/images/hand.png") no-repeat;
-	width: 30px;
-	height: 20px;
-	position: absolute;
-	top: -12px;
-	right: -112px;
-}
-.left_handing{
-	background: url("<?php echo G_GLOBAL_STYLE; ?>/global/login/images/left-handing.png") no-repeat;
-	width: 30px;
-	height: 20px;
-	position: absolute;
-	top: -24px;
-	left: 139px;
-}
-.right_handinging{
-	background: url("<?php echo G_GLOBAL_STYLE; ?>/global/login/images/right_handing.png") no-repeat;
-	width: 30px;
-	height: 20px;
-	position: absolute;
-	top: -21px;
-	left: 210px;
-}
-
-.code{
-margin : -20px -1px 0px 204px;
-}
-</style>
      
 <script type="text/javascript">
 $(function(){
@@ -160,48 +38,66 @@ $(function(){
 });
 </script>
  
-<meta name="GENERATOR" content="MSHTML 11.00.9600.17496"></head> 
+<meta name="GENERATOR" content="MSHTML 11.00.9600.17496">
+<link type="text/css" rel="stylesheet" href="<?php echo G_GLOBAL_STYLE; ?>/global/css/materialize/css/materialize.min.css"  media="screen,projection"/>
+</head> 
 <body>
 
+	<nav>
+    	<div class="nav-wrapper" style="margin-top: -22px;">
+     		<p class="brand-logo" style="margin:0 10px;">DivTeam</p>
+    	</div>
+	</nav>
+
+<div class="container">
+ 	<div class="row" style="margin-top:90px;">
+        <div class="col s6 offset-s3">
+          <div class="card grey lighten-5">
+            <div class="card-content blue-text text-darken-2">
+              <p class="card-title" style="text-align:center;">后台登陆</p>
+               <form action="#" method="post" id="form">
+					<div class="row">
+				        <div class="input-field col s12">
+				          <input id="input-u" name="username" class="ipt validate" value="" type="text">      
+				          <label for="input-u">Username</label>
+				        </div>
+				      </div>
+
+				    <div class="row">
+				        <div class="input-field col s12">
+				          <input class="ipt validate" type="password" id="input-p" name="password" value="">   
+				          <label for="input-p">Password</label>
+				        </div>
+				    </div>
+
+					<div class="row">
+				        <div class="input-field col s12">
+							<input class="ipt validate" type="text" id="input-c" name="code" value="">           
+							<label for="input-c">验证码</label>
+							<?php if(_cfg("web_off")){ ?>
+								<span class="code" style="float: right;">
+									<img width="88px" height="35px" id="checkcode" src="<?php echo WEB_PATH; ?>/api/checkcode/image/80_27/"/>
+								</span>   
+							<?php } ?>
+				        </div>
+				      </div>
 
 
-<div class="top_div"></div>
-<div style="background: rgb(255, 255, 255); margin: -100px auto auto; border: 1px solid rgb(231, 231, 231); border-image: none; width: 400px; height: 200px; text-align: center;">
-<div style="width: 165px; height: 96px; position: absolute;">
-<div class="tou"></div>
-<div style="left:100px;top:-12px;" class="initial_left_hand" id="left_hand"></div>
-<div style="right:-112px;top:-12px" class="initial_right_hand" id="right_hand"></div></div>
- <form action="#" method="post" id="form">
-<p style="padding: 30px 0px 10px; position: relative;">
-<span class="u_logo"></span>         
-<input id="input-u" name="username" class="ipt" placeholder="YYGCMS管理账号" value="" type="text"> 
-</p>
-<p style="position: relative;">
-<span class="p_logo"></span>         
-<input class="ipt" type="password" id="input-p" name="password" placeholder="YYGCMS管理密码" value="">   
-</p>
-<?php if(_cfg("web_off")){ ?>
-</br>
-<p style="position: relative;">
-<span class="code">
-<img width="88px" height="35px" id="checkcode" src="<?php echo WEB_PATH; ?>/api/checkcode/image/80_27/"/>
-</span>   
-<?php } ?>
-<input class="ipt" type="text" id="input-c" name="code" placeholder="YYGCMS验证码" value="">   
-</p>
-<div style="height: 50px; line-height: 50px; margin-top: 30px; border-top-color: rgb(231, 231, 231); border-top-width: 1px; border-top-style: solid;">
-<p style="margin: 0px 35px 20px 45px;"> 
-<span style="float: right;">
-<input style="background: rgb(0, 142, 173); padding: 7px 10px; border-radius: 4px; border: 1px solid rgb(26, 117, 152); border-image: none; color: rgb(255, 255, 255); font-weight: bold;" type="button" id="form_but"  value="YYGCMS登录" />
-</span>         
-</p>
- </form>
+				      <div class="row">
+				      <a class="waves-effect waves-light btn" id="form_but"  value="登录">登陆</a>
+				 </form>
+            </div>
+          </div>
+        </div>
+    </div>
 </div>
-</div>
+
+
 
 <script src="<?php echo G_GLOBAL_STYLE; ?>/global/js/jquery-1.8.3.min.js"></script>
 <script src="<?php echo G_GLOBAL_STYLE; ?>/global/js/global.js"></script>
 <script src="<?php echo G_PLUGIN_PATH; ?>/layer/layer.min.js"></script>
+ <script type="text/javascript" src="<?php echo G_GLOBAL_STYLE; ?>/global/css/materialize/js/materialize.min.js"></script>
 
 <script type="text/javascript">
 var loading;
@@ -267,13 +163,14 @@ function success(data){
 	if(!obj.error){	
 		window.location.href=obj.text;
 	}else{
-		$.layer({
-			type :0,
-			area : ['auto','auto'],
-			title : ['信息',true],
-			border : [5 , 0.5 , '#7298a6', true],
-			dialog:{msg:obj.text}
-		});
+		// $.layer({
+		// 	type :0,
+		// 	area : ['auto','auto'],
+		// 	title : ['信息',true],
+		// 	border : [5 , 0.5 , '#7298a6', true],
+		// 	dialog:{msg:obj.text}
+		// });
+		Materialize.toast(obj.text, 4000)
 		var checkcode=document.getElementById('checkcode');
 		var src=checkcode.src;
 			checkcode.src='';
